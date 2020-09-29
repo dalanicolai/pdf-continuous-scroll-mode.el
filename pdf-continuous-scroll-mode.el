0069db386a2326f6d9787ef046ea726750b7d7c4
Daniel Nicolai <dalanicolai@gmail.com>
2020-09-29 08:19:36 +0200

    Add header required for package (e.g. quelpa)

--- pdf-continuous-scroll-mode.el
+++ pdf-continuous-scroll-mode.el
@@ -1,3 +1,31 @@
+;;; pdf-continuous-scroll-mode.el --- Continuous scroll minor mode for pdf-tools  -*- lexical-binding: t; -*-
+;; Copyright (C) 2020  Daniel Laurens Nicolai
+
+;; Author: Daniel Laurens Nicolai <dalanicolai@gmail.com>
+;; Version: 0
+;; Keywords: pdf-tools,
+;; Package-Requires: ((emacs "27.1"))
+;; URL: https://github.com/dalanicolai/pdf-continuous-scroll-mode.el
+
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage:
+
+;;; Code:
 (defun pdf-cscroll-window-dual-p ()
   "Return t if current scroll window status is dual, else nil."
     (or (equal 'upper (alist-get 'pdf-scroll-window-status (window-parameters)))
